870b1251e2a480cdfb5f65f40c55097b06b03b34
tanmay-nemade01 <152612566+tanmay-nemade01@users.noreply.github.com>
2023-12-26 15:51:55 +0530

    Update DDL_to_CSV.py

--- pages/DDL_to_CSV.py
+++ pages/DDL_to_CSV.py
@@ -5,4 +5,15 @@ ddl = st.text_input("Enter the DDL")
 ddl_list = ddl.split(', ')
 for i in range(len(ddl_list)):
   ddl_list[i] = ddl_list[i].lstrip()
+
+column_names = []
+column_types = []
+
+for element in ddl_list:
+  name, type = element.split(" ")
+  column_names.append(name)
+  column_types.append(type)
+
 ddl_list
+column_names
+column_types
