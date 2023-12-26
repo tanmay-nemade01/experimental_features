1efaadb3331f78a5d15467f34bfeb1fbd2a135a5
tanmay-nemade01 <152612566+tanmay-nemade01@users.noreply.github.com>
2023-12-26 16:10:49 +0530

    Update DDL_to_CSV.py

--- pages/DDL_to_CSV.py
+++ pages/DDL_to_CSV.py
@@ -14,6 +14,20 @@ for element in ddl_list:
   column_names.append(name)
   column_types.append(type)
 
-ddl_list
-column_names
-column_types
+csv_content = ""
+csv_writer = csv.writer(st.empty())  # Create a temporary CSV writer object
+csv_writer.writerow(["Column Name", "Column Type"])
+for name, type in zip(column_names, column_types):
+    csv_writer.writerow([name, type])
+
+# Get the CSV content as a string
+csv_content = csv_writer.out.getvalue().decode("utf-8")
+
+# Offer the download button
+if st.button("Download CSV"):
+    st.download_button(
+        label="Download Separated Lists",
+        data=csv_content,
+        file_name="separated_lists.csv",
+        mime="text/csv",
+    )
