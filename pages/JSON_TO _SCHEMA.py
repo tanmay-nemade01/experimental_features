651c96b5a47cff61824b5d15eabea52cfc70fea2
tanmay-nemade01 <152612566+tanmay-nemade01@users.noreply.github.com>
2024-01-03 14:46:51 +0530

    Update JSON_TO _SCHEMA.py

--- pages/JSON_TO _SCHEMA.py
+++ pages/JSON_TO _SCHEMA.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import json
 import pandas as pd
+import csv
+import io
+
 
 st.title("JSON Data Extractor")
 
@@ -25,6 +28,25 @@ if uploaded_file is not None:
     df = pd.DataFrame(data)
 
     st.dataframe(df)
+    csv_buffer = io.StringIO()
+    csv_writer = csv.writer(csv_buffer)  # Create a CSV writer on the buffer
+    
+    # Write the CSV content to the buffer
+    csv_writer.writerow(["COLUMN_NAME", "TYPE"])
+    for name, type in zip(name_list, type_list):
+        csv_writer.writerow([name, type])
+    
+    # Get the CSV content as a string
+    csv_content = csv_buffer.getvalue()
+    
+    # Offer the download button
+    if st.button("Download CSV"):
+        st.download_button(
+            label="Download DDL Excel",
+            data=csv_content,
+            file_name="separated_lists.csv",
+            mime="text/csv",
+        )
 
   except json.JSONDecodeError as e:
     st.error("Invalid JSON file. Please upload a valid JSON file.")
