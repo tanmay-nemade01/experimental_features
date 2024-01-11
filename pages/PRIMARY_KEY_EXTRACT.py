69d831b0c9e9ad5eef7252feaf0e9feb65864aaa
tanmay-nemade01 <152612566+tanmay-nemade01@users.noreply.github.com>
2024-01-12 05:28:51 +0530

    Update PRIMARY_KEY_EXTRACT.py

--- pages/PRIMARY_KEY_EXTRACT.py
+++ pages/PRIMARY_KEY_EXTRACT.py
@@ -21,46 +21,7 @@ if ddl is not None:
 
   column_names
   column_types
-  
-  for i in range(len(column_types)):
-      if column_types[i] == 'VARCHAR(16777216)':
-          column_types[i] = 'STRING'
-      if column_types[i] == 'TIMESTAMP_NTZ':
-          column_types[i] = 'TIMESTAMP'
-  
-  data = {
-    "COLUMN_NAME" : column_names,
-    "TYPE" : column_types
-  }
-  
-  df = pd.DataFrame(data)
-  
-  st.dataframe(df)
-  
-  csv_buffer = io.StringIO()
-  csv_writer = csv.writer(csv_buffer)  # Create a CSV writer on the buffer
-  
-  # Write the CSV content to the buffer
-  csv_writer.writerow(["COLUMN_NAME", "TYPE"])
-  for name, type in zip(column_names, column_types):
-      csv_writer.writerow([name, type])
-  
-  # Get the CSV content as a string
-  csv_content = csv_buffer.getvalue()
-  
-  # Offer the download button
-  # if st.button("Download CSV"):
-  #     st.download_button(
-  #         label="Download DDL Excel",
-  #         data=csv_content,
-  #         file_name="separated_lists.csv",
-  #         mime="text/csv",
-  #     )
-  st.download_button(
-      label="Download",
-      data=csv_content,
-      file_name="separated_lists.csv",
-      mime="text/csv",
-  )
+  str1 = str(column_names)
+  str1
 
 st.image('https://github.com/tanmay-nemade01/experimental_features/blob/main/screenshots/DDL%20Screenshot.png?raw=true')
